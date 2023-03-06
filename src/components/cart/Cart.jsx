b6752b9edecb167dd8a1eb5e16b61c49d89b919b
Leandro Romero <romerodisind@gmail.com>
2023-03-06 09:07:49 -0300

    style Cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import { ItemList } from "../itemList/ItemList"
 import { useCarritoContext } from "../../context/CarritoContext"
+import './Cart.css'
 
 export const Cart = () => {
   const { carrito, emptyCart, totalPrice } = useCarritoContext()
@@ -10,19 +11,26 @@ export const Cart = () => {
       {
         carrito.length === 0
         ?
-        <>
-          <h2>Carrito vacio</h2>
-          <Link className="nav-link" to={"/"}><button className="btn btn-primary">Continuar comprando</button></Link>
-        </>
+        <div className='emptyCart'>
+          <h2>¡UPS! El carrito esta vacio.</h2>
+          <Link to={"/"}>
+            <button className="continueBuy">Continuar comprando</button>
+          </Link>
+        </div>
         :
-        <div className="container cartContainer">
-              <ItemList prods={carrito} plantilla="ItemCart"/>
-              <div className="divButtons">
-                <p>Resumen de la compra:$ ${new Intl.NumberFormat('de-DE').format(totalPrice())}</p>
-                <button className="btn btn-danger" onClick={() => emptyCart()}>Vaciar Carrito</button>
-                <Link className="nav-link" to={"/"}><button className="btn btn-dark">Continuar comprando</button></Link>
-                <Link className="nav-link" to={"/checkout"}><button className="btn btn-dark">Finalizar Compra</button></Link>
-              </div>
+        <div>
+          <div className="contentCart">
+          <ItemList prods={carrito} plantilla="ItemCart"/>
+          <div className="divButtons">
+            <div className="divClearCart">
+            <button className="clearCart" onClick={() => emptyCart()}>Eliminar todo</button>
+            </div>
+            <Link className="divContinueBuy" to={"/"}><button className="continueBuy">Continuar comprando</button></Link>
+            <h5 className='textTotal'>TOTAL: $ {new Intl.NumberFormat('de-DE').format(totalPrice())}</h5>
+            <Link className="divEndBuy" to={"/checkout"}><button className="endBuy">Finalizar Compra</button></Link>
+
+          </div>
+        </div>
           </div>
       }
     </>
