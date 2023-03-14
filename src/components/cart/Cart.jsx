7a04f18ea5f658bbf6f1bcd1a096c7ceb8676083
Leandro Romero <romerodisind@gmail.com>
2023-03-14 11:08:39 -0300

    implementation color darkMode in cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -4,14 +4,18 @@ import { Link} from 'react-router-dom'
 import { ItemList } from '../itemList/ItemList'
 // import context carrito
 import { useCarritoContext } from '../../context/CarritoContext'
+// import context dark mode
+import { useDarkModeContext } from '../../context/DarkModeContext';
+
 // import css
 import './Cart.css'
 
 export const Cart = () => {
+  const {darkMode} = useDarkModeContext()
   const { carrito, emptyCart, totalPrice } = useCarritoContext()
   
   return (
-    <div className='cartContent'>
+    <div className={'cartContent'}>
       {carrito.length === 0
       ?
       <div className='emptyCart'>
@@ -22,13 +26,13 @@ export const Cart = () => {
       <div>
         <div className="contentCart">
           <ItemList prods={carrito} plantilla="ItemCart"/>
-          <div className="divButtons">
+          <div className={`divButtons ${darkMode && "divButtonsDark"}`}>
             <div className="divClearCart">
-              <button className="clearCart" onClick={() => emptyCart()}>Eliminar todo</button>
+              <button className={`clearCart ${darkMode && "clearCartDark"}`} onClick={() => emptyCart()}>Eliminar todo</button>
             </div>
-            <Link className="divContinueBuy" to={"/"}><button className="continueBuy">Continuar comprando</button></Link>
-            <h5 className='textTotal'>TOTAL : ${new Intl.NumberFormat('de-DE').format(totalPrice())}</h5>
-            <Link className="divEndBuy" to={"/checkout"}><button className="endBuy">Finalizar Compra</button></Link>
+            <Link className="divContinueBuy" to={"/"}><button className={`continueBuy ${darkMode && "continueBuyDark"}`}>Continuar comprando</button></Link>
+            <h5 className={`textTotal ${darkMode && "textTotalDark"}`}>TOTAL : ${new Intl.NumberFormat('de-DE').format(totalPrice())}</h5>
+            <Link className="divEndBuy" to={"/checkout"}><button className={`endBuy ${darkMode && "endBuyDark"}`}>Finalizar Compra</button></Link>
           </div>
         </div>
       </div>}
